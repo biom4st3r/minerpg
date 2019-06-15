6371abb13f30ff7cc7609a96ea17f2d4391c6bf6
biom4st3r <biom4st3r@gmail.com>
2019-06-14 20:49:20 -0400

    * Barbarian class

---
+++ src/main/java/com/biom4st3r/minerpg/impl/abilities/barbarian/RageAbility.java
@@ -0,0 +1,26 @@
+package com.biom4st3r.minerpg.impl.abilities.barbarian;
+
+import com.biom4st3r.minerpg.api.Ability;
+import com.biom4st3r.minerpg.mixin.RPGPlayer;
+
+import net.minecraft.util.Identifier;
+
+public class RageAbility extends Ability {
+
+    public RageAbility(Identifier name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void doAbility() {
+
+    }
+
+    @Override
+    public void applyCost(RPGPlayer player) {
+
+    }
+
+    
+}
