b1bcdf3a70fc8fb2bdf6760ec178595789707a47
biom4st3r <biom4st3r@gmail.com>
2019-06-18 23:47:49 -0400

    * End of Day

--- src/main/java/com/biom4st3r/minerpg/util/RPGPlayer.java
+++ src/main/java/com/biom4st3r/minerpg/util/RPGPlayer.java
@@ -1,7 +1,25 @@
 package com.biom4st3r.minerpg.util;
 
+import java.util.List;
+
+import com.biom4st3r.minerpg.api.Stat;
+import com.biom4st3r.minerpg.gui.ComponentContainer;
+
+import net.minecraft.inventory.BasicInventory;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.nbt.ListTag;
+
 public interface RPGPlayer
 {
+    public ComponentContainer getComponentContainer();
+
+    public void setComponentContainer(ComponentContainer cc);
+
+    public List<Stat> getStats();
+
+    public BasicInventory fromTags(ListTag lt, int size);
+
+    public ListTag toTags(Inventory bi);
 
     
 }
