c06b19efbe7321abf3ebb97ac4096cbafcb43a40
biom4st3r <biom4st3r@gmail.com>
2019-06-27 17:25:07 -0400

    * Backup before Making RpgComponent

--- src/main/java/com/biom4st3r/minerpg/ClientInit.java
+++ src/main/java/com/biom4st3r/minerpg/ClientInit.java
@@ -1,11 +1,19 @@
 package com.biom4st3r.minerpg;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import com.biom4st3r.minerpg.api.Stat.Stats;
 import com.biom4st3r.minerpg.gui.RPGMenu;
+import com.biom4st3r.minerpg.util.RPGPlayer;
 
 import io.netty.buffer.Unpooled;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
+import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.network.packet.CustomPayloadC2SPacket;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.PacketByteBuf;
 
 public class ClientInit implements ClientModInitializer
@@ -17,6 +25,18 @@ public class ClientInit implements ClientModInitializer
         ScreenProviderRegistry.INSTANCE.registerFactory(
             MineRPG.COMPONENT_BAG_ID, 
             RPGMenu::new);
+
+        ClientSidePacketRegistry.INSTANCE.register(new Identifier("updaterpgstats"), (context,buffer) ->
+        {
+            RPGPlayer player = ((RPGPlayer)MinecraftClient.getInstance().player);
+            HashMap<Stats, Integer> map = player.getStats();
+            for(Stats stat: Stats.values())
+            {
+                map.put(stat, (int)buffer.readByte());
+            }
+            player
+            
+        });
     }
     public static CustomPayloadC2SPacket openRpgMenu()
     {
